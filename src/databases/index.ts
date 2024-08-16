89c770112ef677e55dcf072302f7ceada7d4c820
Hrach Martirosyan <hrach.hmart@gmail.com>
2024-08-16 12:04:17 +0200

    Update index.ts

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -3,7 +3,7 @@ import { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } from '@config
 let credentials = '';
 
 if (DB_USERNAME && DB_PASSWORD) {
-  credentials = `${DB_USERNAME}:${DB_PASSWORD}@`;
+  credentials = `${DB_USERNAME}:${encodeURIComponent(DB_PASSWORD)}@`;
 }
 
 export const dbConnection = {
