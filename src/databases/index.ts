756c72d9818c7d0f7e5ebea20b08891aabd062d8
Hrach Martirosyan <hrach.hmart@gmail.com>
2024-08-16 11:50:28 +0200

    Added credentials support to mongodb

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,7 +1,13 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+
+let credentials = '';
+
+if (DB_USERNAME && DB_PASSWORD) {
+  credentials = `${DB_USERNAME}:${DB_PASSWORD}@`;
+}
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: `mongodb://${credentials}${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
